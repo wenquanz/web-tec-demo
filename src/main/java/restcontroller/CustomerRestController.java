7cef294f4b1306475c44cc9d6438de5c5104abfb
zhouwenquan <wenquanz@gaotime.com>
2017-12-27 16:50:47 +0800

    modify xml config, using mapper scan, service scan. add a servlet

--- src/main/java/restcontroller/CustomerRestController.java
+++ src/main/java/restcontroller/CustomerRestController.java
@@ -1,35 +1,30 @@
-package controller;
+package restcontroller;
 
 import model.Customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import service.CustomerService;
 
-@Controller
-@RequestMapping("/customer")
-public class CustomerController {
+
+@RestController
+@RequestMapping("customer")
+public class CustomerRestController {
 	
 	@Autowired
 	private CustomerService customerService;
 	
-
-	/*public void setCustomerService(CustomerService customerService) {
-		this.customerService = customerService;
-	}*/
-
-	@RequestMapping("/get")
+	@RequestMapping("get")
 	public @ResponseBody String home(){
 		return "hello";
 	}
 	
-	@RequestMapping("/{customercode}")
+	@RequestMapping("{customercode}")
 	public @ResponseBody Customer get(@PathVariable String customercode){
 		Customer customer = customerService.get(customercode);
 		return customer;
--- src/main/java/service/CustomerServiceImpl.java
+++ src/main/java/service/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import mapper.CustomerMapper;
 import model.Customer;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
 	
 	@Autowired
